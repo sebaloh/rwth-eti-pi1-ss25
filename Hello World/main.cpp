5abd9b51e401903413cc24dbe08a6d7bb13b186c
sebaloh <86727645+sebaloh@users.noreply.github.com>
2025-05-12 18:38:30 +0200

    add Hello World

---
+++ Hello World/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+
+	int max;
+	max = 5;
+
+	for (int var = 0; var < max; ++var) {
+		std::cout << var << std::endl;
+	}
+
+	return 0;
+}
