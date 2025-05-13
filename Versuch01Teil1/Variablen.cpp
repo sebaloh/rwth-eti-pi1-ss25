ba190bebc8843ad337de0f38bee56d9e39bce566
sebaloh <86727645+sebaloh@users.noreply.github.com>
2025-05-13 11:13:09 +0200

    add 1.6.1.5c

--- Versuch01Teil1/Variablen.cpp
+++ Versuch01Teil1/Variablen.cpp
@@ -12,18 +12,9 @@
 
 int main()
 {
-    int spielfeld[2][3];
+    int iZweite = 1;
     
-    spielfeld[0][0] = 1;
-    spielfeld[0][1] = 2;
-    spielfeld[0][2] = 3;
-
-    spielfeld[1][0] = 4;
-	spielfeld[1][1] = 5;
-	spielfeld[1][2] = 6;
-
-    std::cout << spielfeld[0][0] << " " << spielfeld[0][1] << " " << spielfeld[0][2] << std::endl;
-    std::cout << spielfeld[1][0] << " " << spielfeld[1][1] << " " << spielfeld[1][2] << std::endl;
+    std::cout << iZweite << std::endl;
     
     return 0;
 }
