b57e5d579838a62686f8dba1408d2b062e8f914d
sebaloh <86727645+sebaloh@users.noreply.github.com>
2025-05-13 11:40:04 +0200

    add 1.6.1.7

--- Versuch01Teil1/Variablen.cpp
+++ Versuch01Teil1/Variablen.cpp
@@ -12,18 +12,24 @@
 
 int main()
 {
-    std::string sVorname;
-    int iName1;
-    int iName2;
+	std::string sVorname;
+	int iName1;
+	int iName2;
+	int iPosition1;
+	int iPosition2;
 
-    std::cout << "Vorname: " << std::endl;
-    std::cin >> sVorname;
+	std::cout << "Vorname: " << std::endl;
+	std::cin >> sVorname;
 
-    iName1 = sVorname[0];
-    iName2 = sVorname[1];
+	iName1 = sVorname[0];
+	iName2 = sVorname[1];
 
-    std::cout << "Erster Buchstab: " << iName1 << std::endl;
-    std::cout << "Zweiter Buchstabe: " << iName2 << std::endl;
+	// A = 65, a = 97 -> diff = 32
+	iPosition1 = (iName1 - 64) % 32;
+	iPosition2 = (iName2 - 64) % 32;
+
+	std::cout << "Position erster Buchstabe im Alphabet: " << iPosition1 << std::endl;
+	std::cout << "Position zweiter Buchstabe im Alphabet: " << iPosition2 << std::endl;
     
     return 0;
 }
