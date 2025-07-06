854b000c3771edb87d675da998d4692f7f9a0972
sebaloh <86727645+sebaloh@users.noreply.github.com>
2025-07-06 16:23:42 +0200

    add 9.4.1 [msgBox]

--- Streetplaner/mainwindow.cpp
+++ Streetplaner/mainwindow.cpp
@@ -4,6 +4,8 @@
 #include <QString>
 #include <QDebug>
 #include <QLineEdit>
+#include <QMessageBox>
+#include <QRandomGenerator>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -21,15 +23,30 @@ void MainWindow::on_pushButton_eingabe_clicked()
 {
     QString lieblingsstadt = ui->lineEdit_eingabe->text();
 
+    int random = QRandomGenerator::global()->bounded(1000);
+
     bool ok;
     int i = lieblingsstadt.toInt(&ok);
+
+    QMessageBox msgBox;
+    msgBox.setStandardButtons(QMessageBox::Discard);
+    msgBox.setDefaultButton(QMessageBox::Discard);
 
     if (ok)
     {
         qDebug() << QString("Zahl (+4): %1").arg(i+4);
+        QString plusVier = QString("%1").arg(i+4);
+
+        msgBox.setText("Zahl (+4): ");
+        msgBox.setInformativeText(plusVier);
     }
     else
     {
         qDebug() << QString("Lieblingsstadt: %1").arg(lieblingsstadt);
+
+        msgBox.setText("Lieblingsstadt:");
+        msgBox.setInformativeText(lieblingsstadt);
     }
+
+    msgBox.exec();
 }
